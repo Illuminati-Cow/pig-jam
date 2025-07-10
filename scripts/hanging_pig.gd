512e042c6d76d32123415274d808e0e5368d8716
Cole Falxa-Sturken <colefalxa@gmail.com>
2025-07-10 13:47:01 -0700

    Slider intersection wip

--- scripts/hanging_pig.gd
+++ scripts/hanging_pig.gd
@@ -1,1 +1,9 @@
 extends RigidBody3D
+
+@onready var _area = $Area3D
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	if "intersection" in area.name.to_lower():
+		axis_lock_linear_x = true
+		axis_lock_linear_z = false
+		create_tween().tween_property(self, ^"global_position", area.global_position, 0.1)
+		area.set_deferred(&"monitorable", false)
